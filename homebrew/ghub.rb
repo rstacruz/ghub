d80ded0a18b490b6fafab88f9d6f3ca3458fc282
Rico Sta. Cruz <hi@ricostacruz.com>
2015-02-14 12:15:53 +0800

    Update ghub homebrew formula

--- homebrew/ghub.rb
+++ homebrew/ghub.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Ghub < Formula
   homepage 'https://github.com/rstacruz/ghub'
-  version '1.0.1'
+  version '1.0.2'
   url "https://github.com/rstacruz/ghub/archive/v#{version}.tar.gz"
-  sha1 '3cf3a928b3ecb38dd9950cb91adbb2adc97fb81d'
+  sha1 'eebb1151bbdca5ac7bb5a478b26c380670231434'
 
   head do
     url 'https://github.com/rstacruz/ghub.git', :using => :git
